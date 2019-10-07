56e996209ae3e8ad9508782d1cdc83c81eda7445
clemensma <56268428+clemensma@users.noreply.github.com>
2019-10-07 21:11:07 +0200

    Update complete_frame.py

--- complete_frame.py
+++ complete_frame.py
@@ -10,17 +10,29 @@ main_frame = pd.read_pickle('main_frame')
 speed_frame = pd.read_pickle('speed_frame')
 
 def add_speed(main_frame = main_frame, speed_frame = speed_frame, title = 'speed'):
-    length = len(main_frame.index)
-    empty_list = [np.nan] * length
-    main_frame[title] = empty_list
-    print(main_frame)
+
+    main_frame[title] = [np.nan] * len(main_frame.index)
+
+    new_df = pd.DataFrame(columns = main_frame.columns)
+    new_df.loc[0] = [np.nan]*len(main_frame.columns)
+
     for i in range(len(speed_frame.index)):
         name = speed_frame['name'][i]
-        for k in range(len(main_frame.index)):
-            if name == main_frame['name'][k]:
-                main_frame[title][k] = speed_frame[title][i]
-    print(main_frame[title])
+        if name in main_frame['name'].tolist():
+            for k in range(len(main_frame.index)):
+                if name == main_frame['name'][k]:
+                    main_frame[title][k] = speed_frame[title][i]
+        else:
+            new_df1 = pd.DataFrame(columns = main_frame.columns)
+            new_df1.loc[0] = [np.nan]*len(main_frame.columns)
 
+            new_df1['name'][0] = speed_frame['name'][i]
+            new_df1[title][0] = speed_frame[title][i]
+
+            new_df= pd.concat([new_df, new_df1])
+
+    main_frame = pd.concat([main_frame, new_df])
+    print(main_frame.mean())
 
 
 
