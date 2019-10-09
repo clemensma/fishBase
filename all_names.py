32ce74ba13b90ba48e7ee61e0ada711d6056c124
bigpptobi <56268357+bigpptobi@users.noreply.github.com>
2019-10-09 19:29:09 +0200

    Add files via upload
    all names in one data frame (about 19,000)

---
+++ all_names.py
@@ -0,0 +1,28 @@
+import requests
+import lxml.html as lh
+import pandas as pd
+from bs4 import BeautifulSoup
+import numpy as np
+from natsort import index_natsorted, order_by_index
+
+# creating a dataframe containing all names listed on fishbase (https://www.fishbase.de/ComNames/searchResult.php?language=English&country=)
+def list_of_names(fname = 'searchResult.php'): 
+    names = []
+    ulist = []
+    bs = BeautifulSoup(open(fname), features='lxml')
+    for species in bs.findAll('i'):
+        for parents in species.parents:
+            if parents.name == 'td':
+                names.append(str(species.contents))
+    
+    [ulist.append(x) for x in names if x not in ulist] # deleting double values
+    
+    df = pd.DataFrame() # create dataframe 
+    df['name'] = ulist
+
+    df = df.reindex(index=order_by_index(df.index, index_natsorted(df['name'], reverse=False))) # sort alphabetically
+    df = df.reset_index(drop=True)  # fix index
+    return df
+
+result = list_of_names()
+result.to_pickle('list_of_names')
